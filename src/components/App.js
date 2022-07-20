069a81b5ee5d41ca3bdd2ba04c35e14b140cd8fd
rodrigocqb <rodrigocortibarros@gmail.com>
2022-07-20 12:57:11 -0300

    Welcome screen layout done

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import Welcome from "./Welcome";
+
+export default function App() {
+    return (
+        <>
+            <Welcome />
+        </>
+    );
+}
---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+export default function Welcome() {
+    return (
+        <>
+            <div className="welcome">
+                <img src="./img/main-logo.png" alt="" />
+                <h1>ZapRecall</h1>
+                <div className="button">Iniciar Recall!</div>
+            </div>
+        </>
+    );
+}
