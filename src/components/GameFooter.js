592a3ca6fd0d7fb1eaa188d5ab80bf7317654330
rodrigocqb <rodrigocortibarros@gmail.com>
2022-07-21 16:44:33 -0300

    Answer Icons showing correctly

--- src/components/GameFooter.js
+++ src/components/GameFooter.js
@@ -1,9 +1,34 @@
-export default function GameFooter({answerValues}) {
+export default function GameFooter({ answerValues }) {
     console.log(answerValues);
     return (
         <footer>
             <div className="answered">{answerValues.length}/4 CONCLUÍDOS</div>
-            {(answerValues !== []) && <div>{"eae"}</div>}
+            <div className="answer-icons">
+                {(answerValues !== []) && answerValues.map((value, index) => {
+                    switch (value) {
+                        case "red":
+                            return (
+                                <div key={index} className="R">
+                                    <ion-icon name="close-circle"></ion-icon>
+                                </div>
+                            );
+
+                        case "yellow":
+                            return (
+                                <div key={index} className="Y">
+                                    <ion-icon name="help-circle"></ion-icon>
+                                </div>
+                            );
+
+                        case "green":
+                            return (
+                                <div key={index} className="G">
+                                    <ion-icon name="checkmark-circle"></ion-icon>
+                                </div>
+                            );
+                    }
+                })}
+            </div>
         </footer>
     );
 }
