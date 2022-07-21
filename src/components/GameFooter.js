f40a148956aa4839d7c8bd9e09a5eccaba467ca8
rodrigocqb <rodrigocortibarros@gmail.com>
2022-07-21 18:02:56 -0300

    Result showing correctly based on answers

--- src/components/GameFooter.js
+++ src/components/GameFooter.js
@@ -2,6 +2,27 @@ export default function GameFooter({ answerValues }) {
     console.log(answerValues);
     return (
         <footer>
+            {(answerValues.length === 4) &&
+                (answerValues.includes("red") ?
+                    <div className="result">
+                        <div className="result-title">
+                            <img src="./img/sad.png" alt="" />
+                            <span>Putz...</span>
+                        </div>
+                        <div className="result-description">
+                            <p>Ainda faltam alguns...
+                                Mas não desanime!</p>
+                        </div>
+                    </div> :
+                    <div className="result">
+                        <div className="result-title">
+                            <img src="./img/party.png" alt="" />
+                            <span>Parabéns!</span>
+                        </div>
+                        <div className="result-description">
+                            <p>Você não esqueceu de nenhum flashcard!</p>
+                        </div>
+                    </div>)}
             <div className="answered">{answerValues.length}/4 CONCLUÍDOS</div>
             <div className="answer-icons">
                 {(answerValues !== []) && answerValues.map((value, index) => {
