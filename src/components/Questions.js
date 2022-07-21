7fc632d5cc87a3d8bfdcced9cd061c6702f48c88
rodrigocqb <rodrigocortibarros@gmail.com>
2022-07-20 23:07:52 -0300

    Questions registering answer values on div

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -31,13 +31,47 @@ export default function Questions({ id, question, answer }) {
                 <li className="question-open">
                     <p>{answer}</p>
                     <div className="colors">
-                        <div className="red"><p>Não lembrei</p></div>
-                        <div className="yellow"><p>Quase não lembrei</p></div>
-                        <div className="green"><p>Zap!</p></div>
+                        <div className="red" onClick={() => { setQuestionState(<Answered color="red" />) }} ><p>Não lembrei</p></div>
+                        <div className="yellow" onClick={() => { setQuestionState(<Answered color="yellow" />) }} ><p>Quase não lembrei</p></div>
+                        <div className="green" onClick={() => { setQuestionState(<Answered color="green" />) }} ><p>Zap!</p></div>
                     </div>
                 </li>
             </>
         );
+    }
+
+    function Answered({ color }) {
+        switch (color) {
+            case "red":
+                return (
+                    <>
+                        <li className="question-answered R">
+                            <div>{"Pergunta " + id}</div>
+                            <div><ion-icon name="close-circle"></ion-icon></div>
+                        </li>
+                    </>
+                );
+
+            case "yellow":
+                return (
+                    <>
+                        <li className="question-answered Y">
+                            <div>{"Pergunta " + id}</div>
+                            <div><ion-icon name="help-circle"></ion-icon></div>
+                        </li>
+                    </>
+                );
+
+            case "green":
+                return (
+                    <>
+                        <li className="question-answered G">
+                            <div>{"Pergunta " + id}</div>
+                            <div><ion-icon name="checkmark-circle"></ion-icon></div>
+                        </li>
+                    </>
+                );
+        }
     }
 
     return (
